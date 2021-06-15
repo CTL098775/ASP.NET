224a510b76c13f04cdd785a35690fb43e738d5d0
fen1614 <asd82953671@gmail.com>
2021-06-15 16:51:39 +0800

    Readme.txt

--- Diagnosis.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[diagnosis]
-(
-	[Id] INT NOT NULL PRIMARY KEY,
-	[patient_id] varchar(11) NOT NULL,
-	[doc_id]  varchar(10) NOT NULL,
-	[Diagnosticrecords]  text NOT NULL,
-	[Prescriptionsign]  text NOT NULL,
-	[num] int NOT NULL
-)
